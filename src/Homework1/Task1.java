5ad603ee010a25065392205f0aa170954667b104
ruskhabibullin <aem.ruslan@gmail.com>
2024-08-07 08:45:37 +0300

    first commit

---
+++ src/Homework1/Task1.java
@@ -0,0 +1,13 @@
+package Homework1;/* Написать приложение, которое будет вычислять и выводить значение по формуле:
+a=4*(b+c-1)/2; b и c задаем в коде самостоятельно */
+
+public class Task1{
+     public static void main(String[] args) {
+        int b = 4;
+        int c = 6;
+        int a = (4 * (b + c - 1) / 2);
+
+    System.out.println("a=" + a);
+   }
+ }
+
